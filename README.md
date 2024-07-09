c27508e9776438ada3e2f004aee5bc5507abbd11
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-07-09 07:01:02 +0200

    refactor: cmd web

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 Snippetbox, which lets people paste and share snippets of text
 
 (From the book of "Let's Go!" by Alex Edwards)
+
+## Cookbook
+
+### Command line flags
+
+`go run ./cmd/web -help` allows you to soo a list of all available command line flags
+
+Hint: Use environment variables and flags together:
+
+`export SNIPPETBOX_ADDR=":9999"` and `go run ./cmd/web -addr=$SNIPPETBOX_ADDR`
+
+### Logs
+
+Redirect the standard out stream to an on-disk file when starting the application: `go run ./cmd/web >>/tmp/web.log`
+
+Notes:
+
+1. `>>` will append to the exitsting file instead of truncating it.
+2. `slog.New()` is concurrency safe and can be shared
+3. Be careful if multiple loggers try to write the same log file.
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,44 +2,33 @@ package main
 
 import (
 	"fmt"
-	"html/template" // New import
-	"log"           // New import
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we use ... to pass the contents
-	// of the files slice as variadic arguments.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err) // Use the serverError() helper.
 		return
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err) // Use the serverError() helper.
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -49,16 +38,16 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }
 
-func downloadHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) downloadHandler(w http.ResponseWriter, r *http.Request) {
 	// Warning: http.ServeFile() does not automatically sanitize the file path.
 	// If you’re constructing a file path from untrusted user input,
 	// to avoid directory traversal attacks you must sanitize the input with filepath.Clean() before using it.
